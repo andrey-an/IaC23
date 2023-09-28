c4318f981023476feb0857df2660b35893399c20
andrey-an <gbox8048@gmail.com>
2023-09-28 10:04:05 +0300

    Done home task

---
+++ 2_terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "yc_cloud" {
+  type = string
+  description = "Yandex Cloud ID"
+}
+
+variable "yc_folder" {
+  type = string
+  description = "Yandex Cloud folder"
+}
+
+variable "yc_token" {
+  type = string
+  description = "Yandex Cloud OAuth token"
+}
+
+variable "yc_sa_key" {
+  type = string
+  description = "Yandex Cloud OAuth token"
+}
+
+variable "db_password" {
+  description = "MySQL user pasword"
+}
